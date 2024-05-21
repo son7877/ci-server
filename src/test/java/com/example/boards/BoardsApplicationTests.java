e421fbef03155db47088baad74d0ca2f51e03793
안홍범 <anhongbeom@baaam.local>
2024-05-21 12:28:29 +0900

    test

--- src/test/java/com/example/boards/BoardsApplicationTests.java
+++ src/test/java/com/example/boards/BoardsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.boards;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class BoardsApplicationTests {
 
+	@Value("${spring.datasource.boardName}")
+	String boardName;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void dbBoardNameIsSa(){
+		assertEquals("sa",boardName);
 	}
 
 }
