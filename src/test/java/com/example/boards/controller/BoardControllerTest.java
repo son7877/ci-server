41489e396f4169f895dc2b3c25d51235b54a070f
안홍범 <anhongbeom@baaam.local>
2024-05-22 16:08:57 +0900

    test

--- src/test/java/com/example/boards/controller/BoardControllerTest.java
+++ src/test/java/com/example/boards/controller/BoardControllerTest.java
@@ -75,4 +75,8 @@ class BoardControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.text", Matchers.is("content")));
     }
 
+    @Test
+    void deleteBoard() {
+
+    }
 }
--- src/test/java/com/example/boards/service/BoardServiceImplTest.java
+++ src/test/java/com/example/boards/service/BoardServiceImplTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
